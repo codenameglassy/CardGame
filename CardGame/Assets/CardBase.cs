0bd6f6801346d49687cb70c1edb57a841f240112
codenameglassy <codenameglassy@gmail.com>
2023-05-26 17:17:07 +0545

    g

---
+++ CardGame/Assets/CardBase.cs
@@ -0,0 +1,161 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBase : MonoBehaviour
+{
+    public CardType Type;
+    [HideInInspector]public int Value;
+
+   
+    public bool IsFaceUp { get; private set; }
+    // Enum to represent the card types
+    public enum CardType
+    {
+        Heart,
+        Diamond,
+        Spade,
+        Club
+    }
+
+    public enum CardValue
+    {
+        J,
+        Q,
+        k
+    }
+    public CardValue cardValue;
+    public Sprite[] cardSpritesHeart;
+    public Sprite[] cardSpritesDiamond;
+    public Sprite[] cardSpritesSpade;
+    public Sprite[] cardSpritesClub;
+    public Sprite faceDown;
+    int cardValueIndex;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(EnumStart());
+    }
+
+    IEnumerator EnumStart()
+    {
+       
+        yield return new WaitForEndOfFrame();
+        GameManager.instance.SelectRandomSlot();
+
+        GameManager.instance.currentSlotToSpawn.AddCardToStack(this);
+        ShowCard();
+
+        yield return new WaitForEndOfFrame();
+
+        FlipFaceDown();
+    }
+
+    private void FixedUpdate()
+    {
+       // ShowCard();
+    }
+
+    void SetType()
+    {
+
+        switch (Type)
+        {
+            case CardType.Heart:
+                spriteRenderer.sprite = cardSpritesHeart[cardValueIndex - 11];
+                break;
+
+            case CardType.Diamond:
+                spriteRenderer.sprite = cardSpritesDiamond[cardValueIndex - 11];
+                break;
+
+            case CardType.Spade:
+                spriteRenderer.sprite = cardSpritesSpade[cardValueIndex - 11];
+                break;
+
+            case CardType.Club:
+                spriteRenderer.sprite = cardSpritesClub[cardValueIndex - 11];
+                break;
+        }
+
+    }
+
+    void SetCard()
+    {
+        switch (cardValue)
+        {
+            case CardValue.J:
+                cardValueIndex = 11;
+                Value = 3;
+                SetType();
+
+                //spriteRenderer.sprite = cardSprites[0];
+                break;
+
+            case CardValue.Q:
+                cardValueIndex = 12;
+                Value = 2;
+                SetType();
+
+                // spriteRenderer.sprite = cardSprites[1];
+                break;
+
+            case CardValue.k:
+                cardValueIndex = 13;
+                Value = 1;
+                SetType();
+
+                //  spriteRenderer.sprite = cardSprites[2];
+                break;
+        }
+    }
+    private void ShowCard()
+    {
+
+        SetCard();
+
+
+    }
+    public void HideCard()
+    {
+       // spriteRenderer.sprite = cardSprites[3];
+    }
+
+    public void SelectCard()
+    {
+        GameManager.instance.selectedCard = this;
+    }
+
+    public void MoveCard(Slot mySlot,Slot targetSlot)
+    {
+        if (!IsFaceUp)
+        {
+            return;
+        }
+        mySlot.RemoveCardFromStack(this);
+        targetSlot.AddCardToStack(this);
+    }
+
+    // Method to flip the card face-up
+    public void FlipFaceUp()
+    {
+        IsFaceUp = true;
+        ShowCard();
+        // Enable card functionality here
+    }
+
+    // Method to flip the card face-down
+    public void FlipFaceDown()
+    {
+        IsFaceUp = false;
+        spriteRenderer.sprite = faceDown;
+        // Disable card functionality here
+    }
+}
---
+++ CardGame/Assets/GameManager.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+
+    public CardBase selectedCard;
+    public Slot selectedCardSlot;
+
+    public List<Slot> slotList = new List<Slot>();
+    int slotIndex = 1;
+
+    [HideInInspector]public Slot currentSlotToSpawn;
+
+    public bool hasGameStarted = false;
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10));
+            RaycastHit2D[] hit = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            foreach (RaycastHit2D hit_ in hit)
+            {
+                if (hit_.collider.CompareTag("Slot"))
+                {
+                    if(selectedCard == null)
+                    {
+                        Slot hitSlot = hit_.collider.GetComponent<Slot>();
+
+                        if(hitSlot.stackedCards.Count <= 0)
+                        {
+                            return;
+                        }
+                        hitSlot.GetlastCard();
+                        Debug.Log(hitSlot.gameObject.name);
+                        selectedCardSlot = hitSlot;
+                    }
+                    else if(selectedCard != null)
+                    {
+                        Slot targetSlot = hit_.collider.GetComponent<Slot>();
+                        selectedCard.MoveCard(selectedCardSlot, targetSlot);
+
+                        //clear
+                        selectedCard = null;
+                        selectedCardSlot = null;
+                    }
+                  
+                }
+            }
+
+        }
+    }
+
+    public void SelectRandomSlot()
+    {
+        switch (slotIndex)
+        {
+            case 1:
+                currentSlotToSpawn = slotList[0];
+                slotIndex = 2;
+                break;
+            case 2:
+                currentSlotToSpawn = slotList[1];
+                slotIndex = 3;
+
+                break;
+            case 3:
+                currentSlotToSpawn = slotList[2];
+                slotIndex = 4;
+
+                break;
+            case 4:
+                currentSlotToSpawn = slotList[3];
+                slotIndex = 1;
+
+                break;
+        }
+    }
+
+}
